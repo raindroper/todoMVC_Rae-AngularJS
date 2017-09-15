42e6a0c19c1351f6b2b003710d542ceab06d8f28
Rae <houxiaoyu1993@126.com>
2017-09-15 18:00:21 +0800

    撤销功能（未能实时更新）

--- js/app.js
+++ js/app.js
@@ -5,92 +5,103 @@
   //con			控制器
   //todoList	任务列表
   var app = angular.module("todoApp", ["serviceMOD"]);
-  app.controller("con", ["$scope", "$log", "$location","ser", function ($scope, $log, $location,ser) {
-	$scope.todolists = ser.getLists();
+  app.controller("con", ["$scope",
+	"$log",
+	"$location",
+	"ser",
+	"$window",
+	function ($scope, $log, $location, ser, $window) {
+	  $scope.todolists = ser.getLists();
 
-	//添加任务
-	//newItem	新添加的任务
-	$scope.newItem = "";
-	$scope.add = function () {
-	  //newItem是否为空
-	  if (!$scope.newItem) {
-		return
-	  }
-	  ser.add($scope.newItem);
-	  //置空newItem
+	  //添加任务
+	  //newItem	新添加的任务
 	  $scope.newItem = "";
-	}
-	//删除任务
-	$scope.remove = function (item) {
-	  ser.remove(item);
-	}
-
-
-	//编辑任务
-	$scope.edit = function (id) {
-	  $scope.editId = id;
-	}
-
-
-	//保存任务
-	$scope.save = function () {
-	  $scope.editId = undefined;
-	}
-
-	//批量切换任务状态
-	$scope.toggleAll = function () {
-	  for (var i = 0; i < $scope.todolists.length; i++) {
-		$scope.todolists[i].complete = $scope.selectAll;
+	  $scope.add = function () {
+		//newItem是否为空
+		if (!$scope.newItem) {
+		  return
+		}
+		ser.add($scope.newItem);
+		//置空newItem
+		$scope.newItem = "";
 	  }
-	}
+	  //删除任务
+	  $scope.remove = function (item) {
+		ser.remove(item);
+	  }
 
 
-	//撤销
-	$scope.cancel = function () {
-	  ser.back();
-	}
+	  //编辑任务
+	  $scope.edit = function (id) {
+		$scope.editId = id;
+	  }
 
 
-	//清除已完成
-	$scope.clearCompleted = function () {
-	  ser.clearCompleted();
-	}
+	  //保存任务
+	  $scope.save = function () {
+		$scope.editId = undefined;
+	  }
 
-	//剩余条目
-	$scope.getActive = function () {
-	  var count = 0;
-	  for (var i = 0; i < $scope.todolists.length; i++) {
-		var item = $scope.todolists[i]
-		if (!item.complete && item.if) {
-		  count++
+	  //批量切换任务状态
+	  $scope.toggleAll = function () {
+		for (var i = 0; i < $scope.todolists.length; i++) {
+		  $scope.todolists[i].complete = $scope.selectAll;
 		}
 	  }
-	  return count
-	}
 
-	//显示状态
-	$scope.loac = $location;
-	$scope.$watch("loac.url()", function (n, o) {
-	  switch (n) {
-	    //未完成
-		case "/active":
-		  $scope.isCompleted = {complete: false};
-		  break;
-		//已完成
-		case "/completed":
-		  $scope.isCompleted = {complete: true};
-		  break;
-		//默认
-		default:
-		  $scope.isCompleted = {complete: undefined};
+
+	  //撤销
+	  $scope.cancel = function () {
+		ser.back();
 	  }
-	})
 
-	//状态更改
-	$scope.saveChange = function () {
-	  ser.save();
-	}
-  }])
+
+	  //清除已完成
+	  $scope.clearCompleted = function () {
+		ser.clearCompleted();
+	  }
+
+	  //剩余条目
+	  $scope.getActive = function () {
+		var count = 0;
+		for (var i = 0; i < $scope.todolists.length; i++) {
+		  var item = $scope.todolists[i]
+		  if (!item.complete && item.if) {
+			count++
+		  }
+		}
+		return count
+	  }
+
+	  //显示状态
+	  $scope.loac = $location;
+	  $scope.$watch("loac.url()", function (n, o) {
+		switch (n) {
+			//未完成
+		  case "/active":
+			$scope.isCompleted = {complete: false};
+			break;
+			//已完成
+		  case "/completed":
+			$scope.isCompleted = {complete: true};
+			break;
+			//默认
+		  default:
+			$scope.isCompleted = {complete: undefined};
+		}
+	  })
+
+	  //状态更改
+	  $scope.saveChange = function () {
+		ser.save();
+	  }
+
+	  //监听
+	  var hasRem = $window.localStorage.getItem("hasRemove");
+	  $scope.$watch(hasRem, function (newVal) {
+		ser.clearRemoveList();
+	  })
+	}])
 
 
   // Your starting point. Enjoy the ride!
--- js/services.js
+++ js/services.js
@@ -1,81 +1,87 @@
 (function (angular) {
   var app = angular.module("serviceMOD", []);
-  app.service("ser", ["$window", function ($win) {
-	//获取数据
-	var str = $win.localStorage.getItem("myTodo") || "[]";
-	//初始化已移除列表
-	var removeStr = $win.localStorage.getItem("removeList") || "[]";
-	var hasRemove = JSON.parse(removeStr);
-	//格式化字符串
-	var todoLists = JSON.parse(str);
-	this.getLists = function () {
-	  return todoLists;
-	}
+  app.service("ser", [
+	"$window",
+	"$controller",
+	function ($win, $con) {
+	  //获取数据
+	  var str = $win.localStorage.getItem("myTodo") || "[]";
+	  //初始化已移除列表
+	  var removeStr = $win.localStorage.getItem("hasRemove") || "[]";
+	  var hasRemove = JSON.parse(removeStr);
+	  //格式化字符串
+	  var todoLists = JSON.parse(str);
+	  this.getLists = function () {
+		return todoLists;
+	  }
 
-	//保存数据
-	this.save = function () {
-	  var str = JSON.stringify(todoLists);
-	  $win.localStorage.setItem("myTodo",str);
-	}
+	  //保存数据
+	  this.save = function () {
+		var str = JSON.stringify(todoLists);
+		$win.localStorage.setItem("myTodo", str);
+	  }
 
-	//保存已删除缓存列表
-	this.saveRemoveList = function () {
-	  var removeStr = JSON.stringify(hasRemove);
-	  $win.localStorage.setItem("hasRemove",removeStr)
-	}
+	  //保存已删除缓存列表
+	  this.saveRemoveList = function () {
+		var removeStr = JSON.stringify(hasRemove);
+		$win.localStorage.setItem("hasRemove", removeStr)
+	  }
 
-	//添加数据
-	this.add = function (newItem) {
-	  console.log(todoLists);
-	  //在前面添加newItem
-	  todoLists.unshift({
-		id: (todoLists.length === 0 ? 1 : (todoLists[0].id - 0 + 1)),
-		name: newItem,
-		complete: false,
-		if:true
-	  })
-	  this.save()
-	}
+	  //添加数据
+	  this.add = function (newItem) {
+		console.log(todoLists);
+		//在前面添加newItem
+		todoLists.unshift({
+		  id: (todoLists.length === 0 ? 1 : (todoLists[0].id - 0 + 1)),
+		  name: newItem,
+		  complete: false,
+		  if: true
+		})
+		this.save()
+	  }
 
 
-	//删除数据
-	this.remove = function (item) {
-	  todoLists[todoLists.length-item.id].if = false;
-	  // item.if = false;
-	  this.save();
-	  hasRemove.push(item.id);
-	  this.saveRemoveList();
-	}
+	  //删除数据
+	  this.remove = function (item) {
+		todoLists[todoLists.length - item.id].if = false;
+		// item.if = false;
+		this.save();
+		hasRemove.push(item.id);
+		this.saveRemoveList();
+	  }
 
-	//清除已完成
-	this.clearCompleted = function () {
-	  for (var i = 0; i < todoLists.length; i++) {
-		if (todoLists[i].complete && todoLists[i].if) {
-		  todoLists[i].if = false;
-		  hasRemove.push(todoLists[i].id)
+	  //清除已完成
+	  this.clearCompleted = function () {
+		for (var i = 0; i < todoLists.length; i++) {
+		  if (todoLists[i].complete && todoLists[i].if) {
+			todoLists[i].if = false;
+			hasRemove.push(todoLists[i].id)
+			this.saveRemoveList();
+		  }
+		}
+		this.save();
+	  }
+
+
+	  //撤销功能
+	  this.back = function () {
+		if (hasRemove.length === 0) {
+		  return
+		}
+		todoLists[(todoLists.length - hasRemove[hasRemove.length - 1])].if = true;
+		this.save();
+		hasRemove.pop();
+		this.saveRemoveList();
+	  }
+
+
+	  //撤销缓存最大10条
+	  this.clearRemoveList = function () {
+		if (hasRemove.length >= 10) {
+		  hasRemove.shift();
 		  this.saveRemoveList();
 		}
 	  }
-	  this.save();
-	}
 
-
-	//撤销功能
-	this.back = function () {
-	  if(hasRemove.length === 0){
-	    return
-	  }
-	  todoLists[(todoLists.length - hasRemove[hasRemove.length - 1])].if = true;
-	  this.save();
-	  hasRemove.pop();
-	  this.saveRemoveList();
-	}
-
-
-	//撤销缓存最大10条
-	//   if(hasRemove.length >= 10){
-	//     hasRemove.shift();
-	// 	this.saveRemoveList();
-	//   }
-  }])
+	}])
 })(angular)
